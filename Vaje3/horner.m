909bf974c103976597a801d60440b7f790fd20d9
Benjamin Benčina <benjamin.bencina@gmail.com>
2018-10-15 12:52:09 +0200

    Še začetek ocene napake.

--- Vaje3/horner.m
+++ Vaje3/horner.m
@@ -1,13 +1,22 @@
-function [P] = horner(p, x)
+function [Px, Qx] = horner(p, x)
+    % Px bodo vrednosti polinoma in Qx ocena napake pri Hornerju
     n = size(p, 2);
     m = size(x, 2);
-    P = zeros(m,1);
+    Px = zeros(m,1);
+    Qx = zeros(m,1);
+    u = 0.5 * 2 ^ (1 - 53);
     for j = 1:m;
         y = p(1);
         for i = 2:n
             y = y * x(j) + p(i);
         end
-        P(j) = y;
+        Px(j) = y;
+        
+        q = 0;
+        for i = 1:n
+            q = q + abs(p(i) * x(j)^(n-i));
+        end
+        Qx(j) = 2 * (n-1) * u * q;
     end
 end
 
--- Vaje3/testPlot.m
+++ Vaje3/testPlot.m
@@ -1,4 +1,4 @@
 x = linspace(1.93, 2.07, 1000);
 p = poly(2 * ones(9,1)); %polinom, ki ima 9-kratno nièlo 2
-
-plot(x, horner(p,x))
+[P, Q] = horner(p, x);
+plot(x, P);
