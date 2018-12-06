7caa938f645cbcfc62902784f7afdf00f7914b18
bbencina <benjamin.bencina@gmail.com>
2018-12-06 21:05:03 +0100

    Popravljena naloga4
    Popravljeno kompletno pivotiranje.

--- DomacaNaloga1/Naloga4/lurazcepkp.m
+++ DomacaNaloga1/Naloga4/lurazcepkp.m
@@ -2,18 +2,18 @@ function [L, U, P, Q] = lurazcepkp(A)
     % LU-razcep matrike A kompletno pivotiranje
     
     n = size(A, 1);
-    P = eye(n);
-    Q = eye(n);
+    p = 1:n;
+    q = 1:n;
     for j=1:(n-1)
         % kompletno pivotiranje
-        [e, i_row] = max(max(A(j:n,j:n), [], 2));
-        [e, i_col] = max(max(A(j:n,j:n), [], 1));
-        i_row = i_row + j - 1;
-        i_col = i_col + j - 1;
-        A([j i_row],:) = A([i_row j],:);
-        A(:,[j i_col]) = A(:,[i_col j]);
-        P(:,[j i_row]) = P(:,[i_row j]);
-        Q(:,[j i_col]) = Q(:,[i_col j]);
+        [e, i_row] = max(abs(A(j:n,j:n)));
+        [tmp, s] = max(e);
+        v = i_row(s) + j-1;
+        s = s+j-1;
+        A([j v],:)=A([v j],:);
+        A(:,[j s])=A(:,[s j]);
+        p([j v])=p([v j]);
+        q([j s])=q([s j]);
         for i=(j+1):n
             l = A(i,j) / A(j,j);
             A(i, j) = l;
@@ -24,4 +24,6 @@ function [L, U, P, Q] = lurazcepkp(A)
     end
     L = tril(A,-1) + eye(n);
     U = triu(A);
+    P = eye(n); P=P(p,:);
+    Q = eye(n); Q=Q(:,q);
 end
